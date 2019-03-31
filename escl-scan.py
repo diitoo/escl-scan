1547a6cf89e6354551afb89db93b59e23d88c8cf
diitoo <49107603+diitoo@users.noreply.github.com>
2019-03-31 16:39:53 +0200

    added --info mode

--- escl-scan.py
+++ escl-scan.py
@@ -13,6 +13,7 @@
 import argparse
 import datetime
 import logging
+import os.path
 import requests
 import sys
 import time
@@ -21,13 +22,15 @@ from lxml import etree
 from urllib.parse import urljoin
 
 DEF_NAME = "scan"
-MAX_POLL = 20
+DEF_MAX_WIDTH = 2500
+DEF_MAX_HEIGHT = 3500
+MAX_POLL = 50
 NS_SCAN = "http://schemas.hp.com/imaging/escl/2011/05/03"
 NS_PWG = "http://www.pwg.org/schemas/2010/12/sm"
 SCAN_REQUEST = """
 <?xml version="1.0" encoding="UTF-8"?>
-<scan:ScanSettings xmlns:pwg="http://www.pwg.org/schemas/2010/12/sm" xmlns:scan="http://schemas.hp.com/imaging/escl/2011/05/03">
-  <pwg:Version>2.63</pwg:Version>
+<scan:ScanSettings xmlns:pwg="__NS_PWG__" xmlns:scan="__NS_SCAN__">
+  <pwg:Version>__VERSION__</pwg:Version>
   <pwg:ScanRegions>
     <pwg:ScanRegion>
       <pwg:XOffset>0</pwg:XOffset>
@@ -45,6 +48,7 @@ SCAN_REQUEST = """
 </scan:ScanSettings>
 """.strip()
 
+
 def main(args):
     # logger
     logging.basicConfig(level=(logging.INFO, logging.DEBUG)[args.verbose or args.very_verbose])
@@ -61,25 +65,10 @@ def main(args):
         filename = args.out
     else:
         filename = DEF_NAME + "_" + str(datetime.datetime.now().strftime("%Y%m%d-%H%M%S")) + "." + args.type
-    log.debug("Filename: %s", filename)
-
-    # format
-    if args.type == "jpg":
-        format = "image/jpeg"
-    elif args.type == "pdf":
-        format = "application/pdf"
-    else:
-        error("Invalid type: %s" % args.type)
-    log.debug("Format: %s", format)
-
-    # color mode
-    if args.color_mode == "r24":
-        colorMode = "RGB24"
-    elif args.color_mode == "g8":
-        colorMode = "Grayscale8"
-    else:
-        error("Invalid color mode: %s" % args.color_mode)
-    log.debug("Color mode: %s", colorMode)
+    if not args.info:
+        log.debug("Filename: %s", filename)
+        if os.path.isfile(filename):
+            error("File exists already: %s" % filename)
 
     http = requests.Session()
 
@@ -89,43 +78,19 @@ def main(args):
     resp = http.get(capUrl)
     resp.raise_for_status()
     tree = etree.fromstring(resp.content)
-    log.debug("Scanner: %s", first(tree.xpath("//pwg:MakeAndModel/text()", namespaces={"pwg": NS_PWG})))
-    log.debug("Serial number: %s", first(tree.xpath("//pwg:SerialNumber/text()", namespaces={"pwg": NS_PWG})))
     if args.very_verbose:
         log.debug("Scanner capabilities: %s", str(etree.tostring(tree)).replace("\\n", "\n").replace("\\t", "  "))
 
-    # sanity check: format
+    version = first(tree.xpath("//pwg:Version/text()", namespaces={"pwg": NS_PWG}))
+    makeAndModel = first(tree.xpath("//pwg:MakeAndModel/text()", namespaces={"pwg": NS_PWG}))
+    serialNumber = first(tree.xpath("//pwg:SerialNumber/text()", namespaces={"pwg": NS_PWG}))
+    adminUri = first(tree.xpath("//scan:AdminURI/text()", namespaces={"scan": NS_SCAN}))
     formats = tree.xpath("//pwg:DocumentFormat/text()", namespaces={"pwg": NS_PWG})
-    log.debug("Supported formats: %s", formats)
-    if format not in formats:
-        error("Unsupported format: '%s', supported: %s" % (format, formats))
-
-    # sanity check: color mode
     colorModes = tree.xpath("//scan:ColorMode/text()", namespaces={"scan": NS_SCAN})
-    log.debug("Supported color modes: %s", colorModes)
-    if colorMode not in colorModes:
-        error("Unsupported color mode: '%s', supported: %s" % (colorMode, colorModes))
-
-    # sanity check: resolution
     xResolutions = tree.xpath("//scan:XResolution/text()", namespaces={"scan": NS_SCAN})
     yResolutions = tree.xpath("//scan:YResolution/text()", namespaces={"scan": NS_SCAN})
-    log.debug("Supported x-resolutions: %s", xResolutions)
-    log.debug("Supported y-resolutions: %s", yResolutions)
-    if args.resolution == '':
-        resolution = max([value for value in xResolutions if value in yResolutions])
-    else:
-        resolution = args.resolution
-    log.debug("Resolution: %s", resolution)
-    if resolution not in xResolutions:
-        error("Unsupported x-resolution '%s', supported: %s" % (resolution, xResolutions))
-    if resolution not in yResolutions:
-        error("Unsupported y-resolution '%s', supported: %s" % (resolution, yResolutions))
-
-    # width/height
-    maxWidth = first(tree.xpath("//scan:MaxWidth/text()", namespaces={"scan": NS_SCAN}), 2500)
-    log.debug("Max width: %s", maxWidth)
-    maxHeight = first(tree.xpath("//scan:MaxHeight/text()", namespaces={"scan": NS_SCAN}), 3500)
-    log.debug("Max height: %s", maxHeight)
+    maxWidth = first(tree.xpath("//scan:MaxWidth/text()", namespaces={"scan": NS_SCAN}), DEF_MAX_WIDTH)
+    maxHeight = first(tree.xpath("//scan:MaxHeight/text()", namespaces={"scan": NS_SCAN}), DEF_MAX_HEIGHT)
 
     # query status
     statusUrl = urljoin(args.url, "eSCL/ScannerStatus")
@@ -137,31 +102,89 @@ def main(args):
         log.debug("Scanner status: %s", str(etree.tostring(tree)).replace("\\n", "\n").replace("\\t", "  "))
     status = first(tree.xpath("//pwg:State/text()", namespaces={"pwg": NS_PWG}))
     log.debug("Scanner status: %s", status)
-    if status != "Idle":
-        error("Invalid scanner status: %s" % status)
+
+    # information
+    if args.info:
+        print("Scanner model: %s" % makeAndModel)
+        print("Serial number: %s" % serialNumber)
+        print("Scanner URL:   %s" % args.url)
+        print("Admin URL:     %s" % adminUri)
+        print("Formats:       %s" % ", ".join(formats))
+        print("Color Modes:   %s" % ", ".join(colorModes))
+        print("X-Resolutions: %s" % ", ".join(xResolutions))
+        print("Y-Resolutions: %s" % ", ".join(yResolutions))
+        print("Max width:     %s" % maxWidth)
+        print("Max height:    %s" % maxHeight)
+        print("Status:        %s" % status)
+        sys.exit(0)
+
+    # format
+    if args.type == "jpg":
+        format = "image/jpeg"
+    elif args.type == "pdf":
+        format = "application/pdf"
+    else:
+        error("Invalid type: %s" % args.type)  # should never happen due to argparse
+    log.debug("Format: '%s', supported: %s", format, formats)
+    if format not in formats:
+        error("Unsupported format: '%s', supported: %s" % (format, formats))
+
+    # color mode
+    if args.color_mode == "r24":
+        colorMode = "RGB24"
+    elif args.color_mode == "g8":
+        colorMode = "Grayscale8"
+    else:
+        error("Invalid color mode: %s" % args.color_mode)  # should never happen due to argparse
+    log.debug("Color mode: '%s', supported: %s", colorMode, colorModes)
+    if colorMode not in colorModes:
+        error("Unsupported color mode: '%s', supported: %s" % (colorMode, colorModes))
+
+    # resolution
+    if args.resolution == '':
+        resolution = max([value for value in xResolutions if value in yResolutions])
+    else:
+        resolution = args.resolution
+    log.debug("Resolution: %s, supported X: %s, supported Y: %s", resolution, xResolutions, yResolutions)
+    if resolution not in xResolutions:
+        error("Unsupported x-resolution '%s', supported: %s" % (resolution, xResolutions))
+    if resolution not in yResolutions:
+        error("Unsupported y-resolution '%s', supported: %s" % (resolution, yResolutions))
 
     # start scanning
+    log.debug("Version: %s", version)
+    if status != "Idle":
+        error("Invalid scanner status: %s" % status)
     startUrl = urljoin(args.url, "eSCL/ScanJobs")
-    startReq = SCAN_REQUEST.replace("__FORMAT__", format).replace("__COLORMODE__", colorMode).replace("__RESOLUTION__", resolution).replace("__WIDTH__", maxWidth).replace("__HEIGHT__", maxHeight)
+    startReq = SCAN_REQUEST.replace(
+        "__NS_SCAN__", NS_SCAN).replace(
+        "__NS_PWG__", NS_PWG).replace(
+        "__VERSION__", version).replace(
+        "__FORMAT__", format).replace(
+        "__COLORMODE__", colorMode).replace(
+        "__RESOLUTION__", resolution).replace(
+        "__WIDTH__", maxWidth).replace(
+        "__HEIGHT__", maxHeight)
     if args.very_verbose:
         log.debug("Sending scan request to %s: %s", startUrl, startReq)
     else:
         log.debug("Sending scan request: %s", startUrl)
     resp = http.post(startUrl, startReq, headers={"Content-Type": "text/xml"})
     resp.raise_for_status()
-    resultUrl = urljoin(resp.headers["Location"] + "/", "NextDocument") # status code is 201 so Requests won't follow Location
+    resultUrl = urljoin(resp.headers["Location"] + "/", "NextDocument")  # status code is 201 so Requests won't follow Location
+    log.debug("Result is at %s", resultUrl)
 
     # poll for the result every two seconds, give up after 10 failures
-    counter = 0
+    counter = 1
     while True:
         time.sleep(2)
-        log.debug("Polling: %s", resultUrl)
+        log.debug("Polling [%d]: %s", counter, resultUrl)
         resp = http.get(resultUrl)
         if resp.status_code == 200:
             log.debug("Received result")
             break
         counter += 1
-        if counter >= MAX_POLL:
+        if counter > MAX_POLL:
             error("Giving up after %d attempts to load result, try it manually later: curl -s %s > %s" % (MAX_POLL, resultUrl, filename))
 
     # write result
@@ -170,15 +193,19 @@ def main(args):
         f.write(resp.content)
     print(filename)
 
+
 def first(a, default=None):
     return a[0] if a else default
+
 
 def error(msg):
     print(msg)
     sys.exit(1)
 
+
 if __name__ == "__main__":
     ap = argparse.ArgumentParser(description="A little Python3 script for scanning via the eSCL protocol")
+    ap.add_argument("-i", "--info", action="store_true", help="show scanner information and exit")
     ap.add_argument("-o", "--out", default="", help="output file name [default: " + DEF_NAME + "_<datetime>.<type>]")
     ap.add_argument("-t", "--type", default="jpg", help="desired resulting file type [default: %(default)s]", choices=["jpg", "pdf"])
     ap.add_argument("-r", "--resolution", default="", help="a single value for both X and Y resolution [default: max. available]")
